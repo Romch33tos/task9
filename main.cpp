e26631ec1d3a735e38efd5c3deb7fbddff5f0dbe
Romch33tos <Romch33tos@yandex.ru>
2025-01-24 20:02:37 +0700

    main.cpp
    это была другая задача :)

--- main.cpp
+++ main.cpp
@@ -2,39 +2,47 @@
 
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "russian");
-    int n, m;
-
-    cout << "Введите количество строк матрицы: ";
-    cin >> n;
-    cout << "Введите количество столбцов матрицы: ";
-    cin >> m;
-
-    int matrix[n][m];
-
-    cout << "Введите элементы матрицы:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> matrix[i][j];
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-    }
-
-    int rotated[n][m];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            rotated[j][n - 1 - i] = matrix[i][j];
-        }
-    }
-
-    cout << "Матрица после поворота на 90 градусов по часовой стрелке:" << endl;
-    for (int j = 0; j < m; j++) {
-        for (int i = n - 1; i >= 0; i--) {
-            cout << rotated[j][i] << " ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
         }
         cout << endl;
     }
+}
+
+void printArray(int arr[], int n) {
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    int n;
+
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    printArray(arr, n);
 
     return 0;
 }
